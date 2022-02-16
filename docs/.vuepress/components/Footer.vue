6d19a7feceaa72ac8a2d0531b8b785c75c0f08e3
XGDragon <s_northman@hotmail.com>
2022-02-16 13:03:29 +0100

    added footer

--- docs/.vuepress/components/Footer.vue
+++ docs/.vuepress/components/Footer.vue
@@ -3,5 +3,6 @@
         <p>
             The FastMri collaboration project is co-funded by the PPP Allowance made available by Health~Holland, Top Sector Life Sciences & Health, to stimulate public-private partnerships. 
         </p>
+        <img :src="$withBase(`/footer.png`)" alt="Health Holland" />
     </div>
 </template>
